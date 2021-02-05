6298f52908b054fa1188b9417c9748c61842889a
Arthurcommit <66469924+Arthurcommit@users.noreply.github.com>
2021-02-05 13:18:29 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -2,10 +2,10 @@
 /*
 Page: connexion.php
 */
-session_start(); // à mettre tout en haut du fichier .php, cette fonction propre à PHP servira à maintenir la $_SESSION
+session_start(); 
 if(isset($_POST['connexion'])) { // si le bouton "Connexion" est appuyé
     // on vérifie que le champ "mail" n'est pas vide
-    // empty vérifie à la fois si le champ est vide et si le champ existe belle et bien (is set)
+    // empty vérifie à la fois si le champ est vide et si le champ existe (is set)
     if(empty($_POST['mail'])) {
         echo "Le champ mail est vide.";
     } else {
@@ -31,10 +31,9 @@ if(isset($_POST['connexion'])) { // si le bouton "Connexion" est appuyé
                 echo "Erreur de connexion à la base de données.";
             } else {
                 // on fait maintenant la requête dans la base de données pour rechercher si ces données existent et correspondent:
-                $Requete = mysqli_query($mysqli,"SELECT * FROM membres WHERE mail = '".$mail."' AND mdp = '".md5($MotDePasse)."'");//si vous avez enregistré le mot de passe en md5() il vous suffira de faire la vérification en mettant mdp = '".md5($MotDePasse)."' au lieu de mdp = '".$MotDePasse."'
-                // si il y a un résultat, mysqli_num_rows() nous donnera alors 1
+                $Requete = mysqli_query($mysqli,"SELECT * FROM membres WHERE mail = '".$mail."' AND mdp = '".md5($MotDePasse)."'");
                 // si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
-                // mysqli_num_rows() vérifie la requête dans laBDD 
+                // mysqli_num_rows() vérifie la requête dans la BDD 
                 if(mysqli_num_rows($Requete) == 0) {
                     echo "Le mail ou le mot de passe est incorrect, le compte n'a pas été trouvé.";
                 } else {
